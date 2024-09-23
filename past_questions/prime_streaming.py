02d75a571ffbc54fadc4b6a176491fd2c41c7f2d
Georgios Kokoromytis <geokoko3@outlook.com.gr>
2024-09-23 04:21:58 +0300

    Added comments for question c (K24 python)

--- past_questions/prime_streaming.py
+++ past_questions/prime_streaming.py
@@ -1,14 +1,14 @@
 import math
 class Primes:
     @staticmethod
-    # Time Complexity: O(√1) + O(√2) + ... + O(√n) ---> approximately O(n^(3/2))
+    # Time Complexity: O(sqrt(k))
     def is_prime(k):
         for i in range(2, math.isqrt(k) + 1):
             if k % i == 0:
                 return False
 
         return True
-        
+
     @staticmethod
     def stream():
         yield 2
@@ -16,7 +16,7 @@ class Primes:
         n = 1
         while True:
             c1 = 6*n - 1
-            c2 = 6*n + 1
+            c2 = 6*n + 1 
             if Primes.is_prime(c1):
                 yield c1
             if Primes.is_prime(c2):
@@ -30,10 +30,16 @@ class Primes:
             if count >= N and count <= M:
                 print(prime)
             count += 1
-            if count > M:
+            if count > M: # Loop stops when count exceeds M, so complexity is O(M*)
                 break
 
 Primes.print_primes(1, 9)
 
+# Time Complexity explanation:
+# The prime number theorem states that the number of primes up to any number K is approximately K/log(K). 
+# So, to find the first M primes, we need M = K/log(K) => K = M*log(K)            
+# To check if a number K is prime, we need to check if it is divisible by any number up to sqrt(K). So, complexity of O(sqrt(K)) for every K.
+# This means that the overall complexity is the sum  O(√1) + O(√2) + ... + O(√K) ---> approximately O(K^(3/2)) for searching up to number K.
+# But K = M*log(K) => O((M*log(K))^(3/2)) = O(M^(3/2) * log(K)^(3/2))
 
-            
+
